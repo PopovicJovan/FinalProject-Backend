bbc86daaa387f547ffa4bbc7cef3f30c1491e625
PopovicJovan <jovanp.school06@gmail.com>
2023-10-01 20:06:25 +0200

    finall..i hope

--- projectapp/serializers.py
+++ projectapp/serializers.py
@@ -7,7 +7,7 @@ class UserSerialized(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-        read_only_fields = ["id"]
+        read_only_fields = ["id", "date_joined"]
 
 
 class LogInSerialized(serializers.ModelSerializer):
