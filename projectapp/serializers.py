d5ddb6c670e03c847f4d17e83f8eeedcdde53d3e
PopovicJovan <jovanp.school06@gmail.com>
2023-10-01 22:05:46 +0200

    render

--- projectapp/serializers.py
+++ projectapp/serializers.py
@@ -7,7 +7,7 @@ class UserSerialized(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-        read_only_fields = ["id", "date_joined"]
+        read_only_fields = ["id"]
 
 
 class LogInSerialized(serializers.ModelSerializer):
@@ -24,7 +24,7 @@ class BlogSerialized(serializers.ModelSerializer):
     class Meta:
         model = Blog
         fields = '__all__'
-        read_only_fields = ["average_rate", "id", "date_created", "date_updated"]
+        read_only_fields = ["average_rate", "id"]
 
 
 class RecensionSerialized(serializers.ModelSerializer):
